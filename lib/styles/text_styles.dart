a6b3a2654e8fe8584072011a7f49adffd82235e3
Nam Pham Hoang <nam.phho@gmail.com>
2019-05-16 11:15:41 +0700

    setup

---
+++ lib/styles/text_styles.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+const Color ColorPrimary = const Color(0xff89c541);
+const Color ColorPrimaryLight = const Color(0xff9CCC65);
+const Color ColorBg = const Color(0xffBDBDBD);
+const Color textColor = const Color(0xff212121);
+const Color hintTextColor= const Color(0xff9E9E9E);
